e1a0919dbd5e8caf66d2b4f1100da5ec82768cda
pauluzo <45356331+pauluzo@users.noreply.github.com>
2020-08-26 14:58:19 +0100

    Updates

---
+++ precache-manifest.3cf3d5590f6586165129c14da9b07457.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c990d181aa589d32515883fcdadb9da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f61702b55920e27d8716",
+    "url": "/static/css/main.8493c922.chunk.css"
+  },
+  {
+    "revision": "c35fda5d75dd6c8270cf",
+    "url": "/static/js/2.6b081391.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b081391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61702b55920e27d8716",
+    "url": "/static/js/main.41705a7d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "205a90c306b38f624844fac8dd00d3a7",
+    "url": "/static/media/bgphoto.205a90c3.jpg"
+  },
+  {
+    "revision": "f3084d7057e52b369058983ac9b6f82a",
+    "url": "/static/media/chit_chat.f3084d70.png"
+  },
+  {
+    "revision": "dd24e5cbdc77ae6eac2c09f96de0c293",
+    "url": "/static/media/github_logo.dd24e5cb.png"
+  },
+  {
+    "revision": "56fe8aa902f49bead518a9c6439325b4",
+    "url": "/static/media/gmail_logo.56fe8aa9.png"
+  },
+  {
+    "revision": "0b5caa8d609d249a8be4a691983a4485",
+    "url": "/static/media/leren_app.0b5caa8d.png"
+  },
+  {
+    "revision": "b8b6836f0b8725ffff9b280cf51cd4fa",
+    "url": "/static/media/movie_search.b8b6836f.png"
+  },
+  {
+    "revision": "7441336777556c666742b833b832c414",
+    "url": "/static/media/nubi_website.74413367.png"
+  },
+  {
+    "revision": "abef9cb96749ab186ddce45385b4f8d7",
+    "url": "/static/media/portfolio_site.abef9cb9.png"
+  },
+  {
+    "revision": "459a6ce7db5a12dae3debd15c0627c4f",
+    "url": "/static/media/profile_pic.459a6ce7.jpg"
+  },
+  {
+    "revision": "9569f1deee098da7aedc43f12bd9597e",
+    "url": "/static/media/restaurant_app.9569f1de.png"
+  },
+  {
+    "revision": "395adeb4854f0f740672a70c6b609556",
+    "url": "/static/media/rxjs-logo.395adeb4.png"
+  },
+  {
+    "revision": "d8dd6108afca52ec15490677c32d26f8",
+    "url": "/static/media/treevia-app.d8dd6108.png"
+  },
+  {
+    "revision": "927961e8430fa7f5ed9b6c613ec41b81",
+    "url": "/static/media/twitter_logo.927961e8.png"
+  },
+  {
+    "revision": "5986b605f4d659b0a1b35c2bfc8b602c",
+    "url": "/static/media/vgg_website.5986b605.png"
+  }
+]);
